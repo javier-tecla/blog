472357e383f4cd067daf4b2281b81b5fc1658c39
Javier Borjas <cristman11@gmail.com>
2025-06-04 21:56:38 -0300

    10 - Listar Posts

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\Admin\CategoryController;
 
 Route::resource('categories', CategoryController::class);
+Route::resource('posts', PostController::class);

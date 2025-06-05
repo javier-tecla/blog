80e1a9fdaf0a48ebc80df41ca363d1b3fba4fd4a
Javier Borjas <cristman11@gmail.com>
2025-06-04 21:21:21 -0300

    09 - Insertar Datos de Prueba para Posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -23,5 +24,6 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Category::factory(5)->create();
+        Post::factory(100)->create();
     }
 }
